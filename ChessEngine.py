c76d2e8bd6399b97a7098d9b7d7cc891e009b30a
Ryan-Brosius <ryanbrosius00@gmail.com>
2022-06-21 16:24:58 -0400

    Created basic ChessEngine layout aswell as ChessMain.py file

---
+++ ChessEngine.py
@@ -0,0 +1,27 @@
+## Name - ChessEngine.py
+## Purpose - Class is responsible for storing information about the state of a chess game and valid moves
+## Author - Ryan Brosius
+## Date - 6/21/2022
+
+class GameState():
+    def __init__(self):
+        #8x8 2D list
+        #bR --> Black Rook
+        #bN --> Black Knight
+        #bB --> Black Bishop
+        #bQ --> Black Queen
+        #bK --> Black King
+        #bP --> Black Pawn
+        #-- --> Empty Space
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
---
+++ ChessMain.py
@@ -0,0 +1,6 @@
+## Name - ChessMain.py
+## Purpose - Main driver file, responsible for user input and displaying information
+## Author - Ryan Brosius
+## Date - 6/21/2022
+
+import pygames
