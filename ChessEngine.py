e82fd65206383595cdedd94cd9ee12ba28698947
Ryan-Brosius <ryanbrosius00@gmail.com>
2022-06-27 21:40:36 -0400

    added the ability to undo moves, and added the first basic logic for pawn pieces aswell as a valid move log

--- ChessEngine.py
+++ ChessEngine.py
@@ -3,6 +3,7 @@
 ## Author - Ryan Brosius
 ## Date - 6/21/2022
 
+
 class GameState():
     def __init__(self):
         #8x8 2D list
@@ -29,11 +30,70 @@ class GameState():
     def getBoard(self):
         return self.__board
 
+    def getMoveLog(self):
+        return self.__moveLog
+
+    def getWhiteToMove(self):
+        return self.__whiteToMove
+
     def makeMove(self, move):
         self.__board[move.getStartSq()[0]][move.getStartSq()[1]] = "--"
         self.__board[move.getEndSq()[0]][move.getEndSq()[1]] = move.getPieceMoved()
         self.__moveLog.append(move)
         self.__whiteToMove = not self.__whiteToMove
+
+    def undoMove(self):
+        if len(self.getMoveLog()) > 0:
+            move = self.__moveLog.pop()
+            self.__board[move.getStartSq()[0]][move.getStartSq()[1]] = move.getPieceMoved()
+            self.__board[move.getEndSq()[0]][move.getEndSq()[1]] = move.getPieceCaptured()
+            self.__whiteToMove = not self.__whiteToMove
+
+    def getValidMoves(self):    #All moves when in check
+        return self.getAllPossibleMoves()
+
+    def getAllPossibleMoves(self):  #All moves in general
+        moves = []
+        for row in range(len(self.getBoard())):
+            for col in range(len(self.getBoard()[row])):
+                turn = self.getBoard()[row][col][0]
+                if (turn == "w" and self.__whiteToMove) or (turn == "b" and not self.__whiteToMove):
+                    piece = self.__board[row][col][1]
+                    if piece == "P":
+                        self.getPawnMoves(row, col, moves)
+                    elif piece == "R":
+                        self.getRookMoves(row, col, moves)
+        return moves
+
+    def getPawnMoves(self, row, col, moves):    #Gets all of the valid pawn moves
+        if self.__whiteToMove and row >= 1:  #White pawn moves (SIDE NOTE: DOESNT CALCULATE ON ROW 0)
+            if self.__board[row-1][col] == "--":    #Square above is empty
+                moves.append(Move((row, col), (row-1, col), self.__board))
+                if row == 6 and self.__board[row-2][col] == "--":   #On starting rank, and 2 squares above is empty
+                    moves.append(Move((row, col), (row-2, col), self.__board))
+            if col-1 >= 0:  #Checks to capture a piece from the left
+                if self.__board[row-1][col-1][0] == "b":
+                    moves.append(Move((row, col), (row-1, col-1), self.__board))
+            if col+1 <= 7:  #Checks to capture a piece from the right
+                if self.__board[row-1][col+1][0] == "b":
+                    moves.append(Move((row, col), (row-1, col+1), self.__board))
+
+        elif not self.__whiteToMove and row <= 6:   #black pawn moves (SIDE NOTE: DOESNT CALCULATE ON ROW 7)
+            if self.__board[row+1][col] == "--": #Square below is empty
+                moves.append(Move((row, col), (row+1, col), self.__board))
+                if row == 1 and self.__board[row+2][col] == "--":   #On starting rank, and 2 squares below is empty
+                    moves.append(Move((row, col), (row+2, col), self.__board))
+            if col-1 >= 0:  #checks to capture a piece from the left
+                if self.__board[row+1][col-1][0] == "w":
+                    moves.append(Move((row, col), (row+1, col-1), self.__board))
+            if col+1 <= 7:  #Checks to capture a piece from the right
+                if self.__board[row+1][col+1][0] == "w":
+                    moves.append(Move((row, col), (row+1, col+1), self.__board))
+
+    def getRookMoves(self, row, col, moves):    #Gets all of the valid rook moves
+        pass
+
+
 
 class Move():
 
@@ -51,6 +111,12 @@ class Move():
         self.__endCol = endSq[1]
         self.__pieceMoved = board[self.__startRow][self.__startCol]
         self.__pieceCaptured = board[self.__endRow][self.__endCol]
+        self.__moveID = self.__startRow * 1000 + self.__startCol * 100 + self.__endRow * 10 + self.__endCol
+
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.__moveID == other.__moveID
+        return False
 
     def getStartSq(self):
         return (self.__startRow, self.__startCol)
@@ -60,6 +126,9 @@ class Move():
 
     def getPieceMoved(self):
         return self.__pieceMoved
+
+    def getPieceCaptured(self):
+        return self.__pieceCaptured
 
     def getChessNotation(self):
         return self.getRankFile(self.__startRow, self.__startCol) + self.getRankFile(self.__endRow, self.__endCol)
