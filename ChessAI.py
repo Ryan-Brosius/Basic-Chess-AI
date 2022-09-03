7465e0c2e21f52a261f2af27b47fd051bf144203
Ryan-Brosius <ryanbrosius00@gmail.com>
2022-09-03 18:56:49 -0400

    added greedy algorithm that chooses moves based off of the minmax chess algorithm

--- ChessAI.py
+++ ChessAI.py
@@ -5,5 +5,51 @@
 
 import random
 
+pieceScores ={"K": 0,
+              "Q": 10,
+              "R": 5,
+              "B": 3,
+              "N": 3,
+              "P": 1}
+CHECKMATE = 1000
+STALEMATE = 0
+
 def findRandomMove(validMoves):
     return validMoves[random.randint(0, len(validMoves)-1)]
+
+def findBestMove(gs, validMoves):
+    turnMultiplier = 1 if gs.getWhiteToMove() else -1
+    opponentMinMaxScore = CHECKMATE
+    bestPlayerMove = None
+    random.shuffle(validMoves)
+    for playerMove in validMoves:
+        gs.makeMove(playerMove)
+        opponentMoves = gs.getValidMoves()
+        opponentMaxScore = -CHECKMATE
+        for opponentMove in opponentMoves:
+            gs.makeMove(opponentMove)
+            if gs.getCheckMate():
+                score = -turnMultiplier * CHECKMATE
+            elif gs.getStaleMate():
+                score = STALEMATE
+            else:
+                score = -turnMultiplier * scoreMaterial(gs.getBoard())
+            if score > opponentMaxScore:
+                opponentMaxScore = score
+            gs.undoMove()
+        if opponentMaxScore < opponentMinMaxScore:
+            opponentMinMaxScore = opponentMaxScore
+            bestPlayerMove = playerMove
+        gs.undoMove()
+    return bestPlayerMove
+
+# Score the board on material
+def scoreMaterial(board):
+    score = 0
+    for row in board:
+        for square in row:
+            if square[0] == "w":
+                score += pieceScores[square[1]]
+            elif square[0] =="b":
+                score -= pieceScores[square[1]]
+    return score
